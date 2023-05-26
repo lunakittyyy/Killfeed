bfb5bb5c76270d07bd66d5647ff299250533f55b
Luna <lennon.m.craig@gmail.com>
2023-05-25 23:46:40 -0400

    Initial commit

---
+++ Killfeed/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using UnityEngine;
+using HoneyLib.Events;
+using System.Text.RegularExpressions;
+
+namespace Killfeed
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        string killFeed = "";
+        string latestTag;
+
+        void Start()
+        {
+            Events.InfectionTagEvent += InfectionTagEvent;
+        }
+
+        void OnGUI()
+        {
+            GUI.Box(new Rect(Screen.width - 320, 20, 300, 200), killFeed);
+            if (GUI.Button(new Rect(Screen.width - 70, 235, 50, 30), "Clear"))
+            {
+                killFeed = "";
+            }
+        }
+        
+        void InfectionTagEvent(object sender, InfectionTagEventArgs e)
+        {
+            latestTag = e.taggingPlayer.NickName + " tagged " + e.taggedPlayer.NickName;
+            
+            if (Regex.Matches(killFeed, "\n").Count > 10)
+            {
+                killFeed = latestTag;
+            }
+            else killFeed = killFeed + "\n" + latestTag;
+        }
+    }
+}
---
+++ Killfeed/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Killfeed
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.luna.gorillatag.killfeed";
+        public const string Name = "Killfeed";
+        public const string Version = "1.0.0";
+    }
+}
